0560ec96e75728f70a490d57a4d3caae572cf04c
prajwal <prajwalkreddy17@gmail.com>
2025-07-14 13:58:09 +0530

    add keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,16 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- ctrl + backspace
+vim.keymap.set("i", "<C-h>", "<C-w>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-h>", "<NOP>", { noremap = true, silent = true }) -- NOP => no operation
+vim.keymap.set("c", "<C-h>", "<NOP>", { noremap = true, silent = true })
+
+-- preserves the yanked text even after pasting
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- move between windows
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
